8cdf9a7e5d82968ca6fced7f6dffd66a9f334f67
AtsushiUtsumi <debtgx103@gmail.com>
2023-02-02 14:03:42 +0900

    2個目のコマンドを登録

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,6 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "plugin-test" is now active!');
 
 	// The command has been defined in the package.json file
@@ -20,7 +13,14 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+	disposable = vscode.commands.registerCommand('plugin-test.helloWorld2', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World2 from plugin-test!');
+	});
+
+	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
